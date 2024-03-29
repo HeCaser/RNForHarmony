be31efe0069442ef1dfb914dcef0518bca590da3
hepan <hepan@xueqiu.com>
2024-03-29 16:27:26 +0800

    feat: build and copy scripte

---
+++ shell/dev.sh
@@ -0,0 +1,5 @@
+
+# 遇到权限问题, 执行 chmod u+x ./shell/dev.sh
+react-native bundle-harmony --dev
+cp -R /Users/hepan/DevEcoStudioProjects/RNForHarmony/harmony/entry/src/main/resources/rawfile /Users/hepan/DevEcoStudioProjects/HarmonyForRN/entry/src/main/resources/rawfile
+echo copy success
