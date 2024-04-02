4cd747a0c8e727c5d0aa34755a4945ab184a34c7
hepan <hepan@xueqiu.com>
2024-04-02 11:38:26 +0800

    feat: rn call native , 传递回调函数, 接收 promise 返回

--- pages/bridge/rn_call_native/RnCallNative.tsx
+++ pages/bridge/rn_call_native/RnCallNative.tsx
@@ -1,17 +1,59 @@
-import * as React from 'react';
-import { Text, View, Button, Alert } from 'react-native';
+import React, { useEffect, useState } from "react"
+import { Text, View, Button, Alert, StyleSheet, Keyboard } from 'react-native';
 import SampleTurboModule from './SampleTurboModule';
-
 
 const RnCallNative = () => {
 
-    const bridge = () => {
+    const [result1, setResult1] = useState('')
+    const [result2, setResult2] = useState('')
+    const [result3, setResult3] = useState('')
+
+    const rnLog = () => {
         SampleTurboModule.rnLog('RN Call the rnLog function')
+    }
+
+    const pushStringToHarmony = () => {
+        let result = SampleTurboModule.pushStringToHarmony('rn 传递的参数', 999)
+        setResult1(result)
+    }
+
+    const registerFunction = () => {
+        let start = new Date().getTime()
+        SampleTurboModule.registerFunction((value) => {
+            let end = new Date().getTime()
+            setResult2(`\n${end - start}ms 后, 函数回调 value = ${value}`)
+        })
+    }
+
+    const doAsyncJob = () => {
+        SampleTurboModule.doAsyncJob(true).then((value => {
+            // 成功接收到字符串类型  resolve('resolved on native side')
+            setResult3(`\n promise 成功 value = ${value}`)
+        })).catch((value: object) => {
+            // 失败接收到 object 类型  reject('rejected on native side');
+            setResult3(`\n promise 失败 value = ${JSON.stringify(value)}`)
+        })
+    }
+
+    interface ItemProp {
+        title: string,
+        onPress: () => void
+    }
+
+    const Item = ({ title, onPress }: ItemProp) => {
+        return <View>
+            <View style={{ height: 16 }}></View>
+            <Button title={title} onPress={onPress}></Button>
+        </View>
+
     }
 
     return (
         <View style={{ width: '100%', marginTop: 10, padding: 20, backgroundColor: 'gray' }}>
-            <Button title='调用 Bridge' onPress={bridge}></Button>
+            <Item title={'调用 rnLog, 可传参数, 无返回'} onPress={rnLog}></Item>
+            <Item title={`调用 pushStringToHarmony, 可传两个参数, 接收 string 返回值: ${result1}`} onPress={pushStringToHarmony}></Item>
+            <Item title={`调用 registerFunction, 传递一个回调函数 (value: string) => void  ${result2}`} onPress={registerFunction}></Item>
+            <Item title={`调用 doAsyncJob, 传递 boolen 参数, 回调为 Promise 类型 ${result3}`} onPress={doAsyncJob}></Item>
         </View>
     );
 }
